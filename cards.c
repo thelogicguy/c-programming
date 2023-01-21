9658f5dfee38a5cd68f1483ae7790d53e228c697
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2023-01-21 11:14:37 +0100

    formatted string quotes and compiled code

--- cards.c
+++ cards.c
@@ -12,14 +12,14 @@ int main()
     scanf("%2s", card_name);
 
     int val = 0;
-    if (card_name[0] == "K")
+    if (card_name[0] == 'K')
     {
         val = 10;
-    } else if (card_name[0] == "Q") {
+    } else if (card_name[0] == 'Q') {
         val = 10;
-    }else if (card_name[0] == "J" ) { 
+    }else if (card_name[0] == 'J' ) { 
         val = 10;
-    }else if (card_name[0] == "A"){
+    }else if (card_name[0] == 'A'){
         val = 11;
     }else { val = atoi(card_name);
     }
