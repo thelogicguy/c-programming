4e4c6993a7d9fe478f303df365df34cffdd4f060
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2023-01-22 09:50:21 +0100

    modified to a switch statement

--- cards.c
+++ cards.c
@@ -7,32 +7,43 @@
 
 int main()
 {
-     char card_name[3];
-    puts("Enter the card name:  ");
-    scanf("%2s", card_name);
+    char card_name[3];
+    int count = 0;
 
-    int val = 0;
-    if (card_name[0] == 'K')
-    {
-        val = 10;
-    } else if (card_name[0] == 'Q') {
-        val = 10;
-    }else if (card_name[0] == 'J' ) { 
-        val = 10;
-    }else if (card_name[0] == 'A'){
-        val = 11;
-    }else { val = atoi(card_name);
-    }
+    while (card_name[0] != 'X') {
+        puts("Enter the card name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0])
+        {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
 
-    /* Code checks if the value is 3 to 6 inclusive */
+        case 'X':
+            continue;
 
-    if ((val >= 3) && (val <= 6)) {
-        puts("Count has gone up");
+        default:
+            val = atoi(card_name);
 
-    /* Otherwise checks if the card is 10, J, Q, or K */
+            if ((val <1) || (val > 10)) {
+                puts("I dont understand this value!");
+                continue;
+            }  
+        }
+        if ((val >2) && (val < 7)) {
+                count ++;
 
-    }else if (val == 10 || val == 'J' || val == 'Q' || val == 'K') {
-        puts("Count has gone down");
+            } else if (val == 10) {
+                count --;
+            }
+
+            printf("Current count: %i\n", count);
     }
     return 0;
 }
